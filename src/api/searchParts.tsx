04f0dd636a87fbb20bd59e94c83b71f3853c9957
lepo7763 <ranferplays@gmail.com>
2025-03-22 20:01:58 -0500

    debug2

--- src/api/searchParts.tsx
+++ src/api/searchParts.tsx
@@ -1,21 +1,27 @@
 export const searchParts = async (query: string) => {
-    const res = await fetch ('https://www.api.buildcores.com/api/official/database/parts', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            filters: [],
-            limit: 20,
-            parts_category: 'PCCase',
-            search_query: query,
-            show_disabled_interactive_models: true,
-            show_interactive_first: false,
-            skip: 0,
-            sort: 0,
-        }),
+    const res = await fetch('https://www.api.buildcores.com/api/official/database/parts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        filters: [],
+        limit: 20,
+        part_category: "PCCase",           
+        search_query: "nzxt",               
+        show_disabled_interactive_models: true,
+        show_interactive_first: false,
+        skip: 0,
+        sort: 0
+      }),
     });
+  
     if (!res.ok) {
-        console.error('Failed to fetch:', res.status);
-        throw new Error('API request failed');
+      const errorText = await res.text();
+      console.error('Error response:', errorText);
+      throw new Error(`API request failed: ${res.status}`);
     }
+  
     return res.json();
-};
+  };
+  
